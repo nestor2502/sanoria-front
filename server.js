527f5b8811532d97f6cd862f059a3345f72da9fd
Nestor Vazquez <nestorvazquez@Mac-mini-de-Nestor.local>
2022-01-18 16:26:32 -0600

    restore server file

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 const express = require('express');
-var history = require('connect-history-api-fallback')
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-// Use a fallback for non-root routes (required for Vue router)
-//   NOTE: History fallback must be "used" before the static serving middleware
-app.use(history({
-    // OPTIONAL: Includes more verbose logging
-    verbose: true
-}))
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
