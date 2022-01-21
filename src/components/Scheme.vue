12b279e007dafc29dbe6d2732da87382412ce17d
IsayDBS <isaydamar@ciencias.unam.mx>
2022-01-21 01:28:31 -0600

    Cambios a scheme

--- src/components/Scheme.vue
+++ src/components/Scheme.vue
@@ -6,22 +6,22 @@
     <div>
     <!--Nombre esquema-->
         <h1>
-            Mi esquema
+            My Scheme
         </h1>
     </div>
 
      <div class="row">
         <div class="column">
             <div class="nombre-esquema">
-                <p>Tipo de esquema</p>
+                <p>Type of Schemes</p>
             </div>
         </div>
         <div class="column">
             <!--checkbox-->
-            <input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="...">
+            <!--<input  class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="...">
                 <label class="form-check-label" for="flexCheckDefault">
-                   Solo guardados
-                </label>
+                  <p>Saved</p>
+                </label>-->
         </div>
     </div> 
 
@@ -30,15 +30,15 @@
 
 
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            <div v-for="n in 6" :key="n">
+            <div v-for="post in chunk" :key="post.id">
                 <div class="col">
                     <div class="card shadow-sm">
-                        <img src="../assets/img/food.jpg">
+                        <img :src="post.image">
                             <div class="card-body">
-                                <p class="card-text">Nombre de la comida</p>
-                                    <div class="d-flex justify-content-between align-items-center">
+                                <p class="card-text">{{post.label}}</p>
+                                    <div class="d-flex justify-content-end align-items-center">
                                         <div class="btn-group">
-                                            <button type="button" class="btn btn-sm btn-outline-secondary">Borrar</button>
+                                            <button type="button" class="btn btn-sm btn-delete" @click="aux(post.id)"><i class="far fa-trash-alt"></i></button>
                                         </div>
                                     </div>
                             </div>
@@ -57,8 +57,8 @@
         <span aria-hidden="true">&laquo;</span>
       </a>
     </li>
-    <div v-for="n in 6" :key="n">
-    <li class="page-item"><a class="page-link" href="#">{{n}}</a></li>
+    <div v-for="n in chunks.length" :key="n">
+    <li class="page-item"><a class="page-link" href="#" @click="selectChunk(n)">{{n}}</a></li>
     </div>
     <li class="page-item">
       <a class="page-link" href="#" aria-label="Next">
@@ -72,6 +72,67 @@
 
 
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      posts: [],
+      chunks: [],
+      chunk: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch("https://sanoria-api.herokuapp.com/recipe/schema/1");
+        this.posts = await response.json();
+        this.chunks = this.spliceIntoChunks(this.posts, 6);
+        this.chunk = this.chunks[0];
+      } catch (error) {
+        console.log(error);
+      }
+  },
+    spliceIntoChunks(arr, chunkSize) {
+    const res = [];
+    while (arr.length > 0) {
+        const chunk = arr.splice(0, chunkSize);
+        res.push(chunk);
+    }
+    return res;
+  },
+  aux(id){
+    this.borra(id);
+    this.getData();
+  },
+    selectChunk(n){
+      this.chunk = this.chunks[n - 1];
+  },
+  /**
+  borra(id){
+    fetch("https://sanoria-api.herokuapp.com/recipe/" + id,{
+      method:'DELETE'
+    }).then(response=>{
+      return response.json()
+    }).then(data=> 
+    // this is the data we get after putting our data,
+      console.log(data)
+      );
+    },*/
+  async borra(id){
+    return axios.delete(`https://sanoria-api.herokuapp.com/recipe/${id}`)
+     .then(res => res.data);
+  }
+  },
+  created() {
+    this.getData();
+  },
+};
+
+</script>
 
 <style scoped>
   @import url(../assets/styles/Scheme.css);
